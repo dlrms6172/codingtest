64d44e748f3aaaa0cee41c41a97a19c5fd3edd96
leekun <dlrms617292@gmail.com>
2022-10-24 12:18:48 +0900

    수정

--- src/Solution2.java
+++ src/Solution2.java
@@ -1,23 +1,26 @@
 import java.util.Arrays;
 
 
-
 public class Solution2 {
 
+    static int[] solution(int[] numbers, int num1, int num2){
 
+        int[] answer = Arrays.copyOfRange(numbers,num1,num2+1);
 
+        for(int i=num1-1; i<num2; i++) {
+
+            System.out.print(answer[i]+" ");
+        }
+
+        return answer;
+    }
 
     public static void main(String[] args) {
 
-        int[] numbers = {1,2,3,4,5,};
-        int num1=1;
-        int num2=3;
-        int[] answer = Arrays.copyOfRange(numbers,num1,num2+1);;
+        int[] arr={1,2,3,4,5};
 
+        solution(arr,1,3);
 
-        for(int i=num1-1; i<num2; i++){
-            System.out.print(answer[i]);
-        }
 
 
 
