13b2ff7636e1835f8f4453430f3f7579dca7546b
leekun <dlrms617292@gmail.com>
2022-10-25 16:16:59 +0900

    Stack 사용

--- src/Solution1.java
+++ src/Solution1.java
@@ -1,5 +1,31 @@
+import java.util.Stack;
+
 public class Solution1 {
+
+
+       static int solution(String s) {
+            int answer=0;
+
+           Stack<Integer> stack = new Stack<>();
+
+           for(String t:s.split(" ")){
+               if(t.equals("Z")){
+                   if(!stack.isEmpty())
+                       answer -= stack.pop();
+               }
+               else
+                   answer += stack.push(Integer.parseInt(t));
+           }
+
+           System.out.println(answer);
+            return answer;
+        }
+
+
+
     public static void main(String[] args) {
+
+        solution("10 20 Z 30 40");
 
     }
 }
