1d93f466204266563eeb3cffcec525dcd9de26d5
leekun <dlrms6172@naver.com>
2022-11-11 19:38:55 +0900

    solution6

---
+++ src/Solution6.java
@@ -0,0 +1,19 @@
+public class Solution6 {
+    public static int solution(int n) {
+        int answer = 0;
+
+        for(int i =0; i<=n; i++)
+        {
+            if(i%2==0)
+                answer+=i;
+        }
+
+        System.out.println(answer);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        solution(10);
+    }
+}
