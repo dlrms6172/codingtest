db4e06a85f3feb26d5cfee5ee2e98e89c2ba162c
leekun <dlrms6172@naver.com>
2022-12-04 21:16:05 +0900

    13.제일 작은 수 제거하기

--- src/Solution13.java
+++ src/Solution13.java
@@ -6,22 +6,43 @@ public class Solution13 {
 
     public static int[] solution(int[] arr) {
         int[] answer = {};
-        int min = arr[0];
+        List<Integer> arr2 = new ArrayList<>();
+        int min = 0;
 
-            for (int i = 0; i < arr.length; i++) {
-                if (min > arr[i]) {
-                    min = arr[i];
-                }
-                List<int[]> list = Arrays.asList(arr);
-
+        if(arr.length <= 1){
+            answer = new int[]{-1};
+            for(int i =0; i<answer.length; i++){
+                System.out.println(answer[i]);
             }
+            return answer;
+        }
 
+        for(int i=0; i<arr.length; i++){
+            arr2.add(arr[i]);
+        }
 
-        System.out.println(answer);
+        for(int i=1; i<arr.length; i++){
+            if(arr[min] > arr[i]){
+                min = i;
+            }
+        }
+
+        arr2.remove(min);
+
+        answer = new int[arr2.size()];
+
+        for(int i =0; i<arr2.size(); i++){
+            answer[i] = arr2.get(i);
+        }
+        for(int i =0; i<answer.length; i++){
+            System.out.println(answer[i]);
+        }
         return answer;
     }
     public static void main(String[] args) {
-        int[] arr= {1,2,3,4,5,6};
+        int[] arr= {4,3,2,1};
+        int[] arr2 = {10};
         solution(arr);
+        solution(arr2);
     }
 }
